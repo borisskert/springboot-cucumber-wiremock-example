4bfd3b963e14886ba2d1a34353ee3a1d77070fdf
Boris Skert <boris.skert@gmail.com>
2020-03-06 18:43:13 +0100

    Initial import

---
+++ src/main/java/com/github/borisskert/springbootcucumberwiremockexample/SpringbootCucumberWiremockExampleApplication.java
@@ -0,0 +1,13 @@
+package com.github.borisskert.springbootcucumberwiremockexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCucumberWiremockExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCucumberWiremockExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/borisskert/springbootcucumberwiremockexample/SpringbootCucumberWiremockExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.borisskert.springbootcucumberwiremockexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCucumberWiremockExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
